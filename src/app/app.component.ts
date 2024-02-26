967e4888b03ffd59f21a10c06bc6c74be1456686
unknown <g00438467@atu.ie>
2024-02-26 10:46:49 +0000

    lab4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ListComponent } from './list/list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-app';
+  title:string = 'data-binding-app';
+  count:number = 0;
+  value:boolean = true;
+  hide:boolean = false;
+
+  onClick(){
+    this.count++;
+  }
+
+  ondoubleclick(){
+    if(this.hide == false){
+      this.hide = true;
+    }else{
+      this.hide = false;
+    }
+  }
 }
